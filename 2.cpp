04b050c83bfa5ab340cb9652122b03729cfffd83
Agnes-Li-GitHub <90670390+Agnes-Li-GitHub@users.noreply.github.com>
2022-03-16 17:35:26 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+const int X = 0;
+const int Y = 1;
+
+const int MAXGEOM = 20;
+
+struct AABB {
+  double min[2];  // min[0] = xmin, min[1] = ymin
+  double max[2];  // max[0] = xmax, max[1] = ymax
+
+  // functions
+
+  // Read a point set from user input and calculates AABB
+  void ReadPointSet() {
+    int n;
+    double x, y;
+
+    cin >> n;
+    min[X] = min[Y] = numeric_limits<double>::max();
+    max[X] = max[Y] = numeric_limits<double>::lowest();
+    for (int i = 0; i < n; ++i) {
+      cin >> x >> y;
+      if (x < min[X]) min[X] = x;
+      if (x > max[X]) max[X] = x;
+      if (y < min[Y]) min[Y] = y;
+      if (y > max[Y]) max[Y] = y;
+    }
+  }
+
+  // Read a rectangle from user input and calculates AABB
+  void ReadRectangle() {
+    // --- YOUR CODE HERE (Task 1) ---
+    double x, y, width, height;
+    cin >> x >> y >> width >> height;
+
+    min[X] = min[Y] = numeric_limits<double>::max();
+    max[X] = max[Y] = numeric_limits<double>::lowest();
+    if ((x-width/2) < min[X]) min[X] = (x-width/2);
+    if ((x+width/2) > max[X]) max[X] = (x+width/2);
+    if ((y-height/2) < min[Y]) min[Y] = (y-height/2);
+    if ((y+height/2) > max[Y]) max[Y] = (y+height/2);
+    }
+
+  // Read a circle from user input and calculates AABB
+  void ReadCircle() {
+    // --- YOUR CODE HERE (Task 1) ---
+    double x, y, radius;
+    cin >> x >> y >> radius;
+
+    min[X] = min[Y] = numeric_limits<double>::max();
+    max[X] = max[Y] = numeric_limits<double>::lowest();
+    if ((x-radius) < min[X]) min[X] = (x-radius);
+    if ((x+radius) > max[X]) max[X] = (x+radius);
+    if ((y-radius) < min[Y]) min[Y] = (y-radius);
+    if ((y+radius) > max[Y]) max[Y] = (y+radius);
+    }
+};
+
+// return true if two AABBs overlaps
+//        false otherwise
+bool IsOverlap(AABB p, AABB q) {
+  // --- YOUR CODE HERE (Task 3) ---
+  // x and y overlap
+  if (p.min[0] <= q.max[0] && q.min[0] <= p.max[0] && p.min[1] <= q.max[1] && q.min[1] <= p.max[1]) { 
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+int main()
+{
+  char type;
+
+  AABB boxes[MAXGEOM];
+
+  AABB res; // input AAAB and result AABB
+  // initialize bounds of result AABB
+  res.min[X] = res.min[Y] = numeric_limits<double>::max();
+  res.max[X] = res.max[Y] = numeric_limits<double>::lowest();
+
+  // read geometry type and dispatch to the corresponding
+  // AABB read geomety functions to read from user input handle
+  // and compute AABB
+  // store all AABBs in the array "boxes"
+  int numBoxes = 0;
+
+  cin >> type;
+  while (type != '#') {
+    switch (type) {
+      case 'R':
+        boxes[numBoxes++].ReadRectangle();
+        break;
+      case 'C':
+        boxes[numBoxes++].ReadCircle();
+        break;
+      case 'P':
+        boxes[numBoxes++].ReadPointSet();
+        break;
+      default:
+        break;
+    }
+    cin >> type;
+  }
+
+  // display AABBs for input geometries by going through the array "boxes"
+  for (int i = 0; i < numBoxes; i++) {
+    // --- YOUR CODE HERE (Task 2) ---
+    cout << "AABB " << setw(2) << setfill('0') << i << ": " << boxes[i].min[0] << " " << boxes[i].max[0] << " " << boxes[i].min[1] << " " << boxes[i].max[1] << endl; 
+  }
+  
+  cout << "Isolated AABBs:\n";
+
+  // for each AABB, if it does not overlap with any other AABBs, display its ID
+  // --- YOUR CODE HERE (Task 4) ---
+  bool flag = true;
+  for (int i=0;i<numBoxes;i++){
+    for (int j=0;j<numBoxes;j++){
+      if (IsOverlap(boxes[i], boxes[j]) && (i != j)){ // true = overlap
+        flag = false;
+        break;
+      }
+    }
+    if (flag == true){
+      cout << setw(2) << setfill('0') << i <<endl;
+    }
+    flag = true;
+  }
+  
+  
+  return 0;
+}
